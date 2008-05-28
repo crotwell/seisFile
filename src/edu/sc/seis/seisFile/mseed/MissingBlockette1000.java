f3374eda8345cff354a2bd0880fdd8a30b286ea3
crotwell <crotwell@localhost>
2008-05-28 15:24:11 -0400

    [svn r19799] initial

---
+++ src/edu/sc/seis/seisFile/mseed/MissingBlockette1000.java
@@ -0,0 +1,16 @@
+package edu.sc.seis.seisFile.mseed;
+
+public class MissingBlockette1000 extends SeedFormatException {
+
+    public MissingBlockette1000() {
+        super();
+    }
+
+    public MissingBlockette1000(String s, Throwable cause) {
+        super(s, cause);
+    }
+
+    public MissingBlockette1000(String s) {
+        super(s);
+    }
+}
