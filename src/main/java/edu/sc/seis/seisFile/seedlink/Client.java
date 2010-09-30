c659b29f92c6e765e24b3c87d64e77ccc79a13b8
crotwell <crotwell@localhost>
2010-09-30 11:55:37 -0400

    [svn r21709] clean up, add more cmd line args

--- src/main/java/edu/sc/seis/seisFile/seedlink/Client.java
+++ src/main/java/edu/sc/seis/seisFile/seedlink/Client.java
@@ -4,11 +4,15 @@ import java.io.BufferedOutputStream;
 import java.io.DataOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.StringWriter;
+import java.io.Writer;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import edu.sc.seis.seisFile.BuildVersion;
 import edu.sc.seis.seisFile.mseed.Blockette;
 import edu.sc.seis.seisFile.mseed.DataRecord;
 import edu.sc.seis.seisFile.mseed.SeedFormatException;
@@ -22,6 +26,8 @@ public class Client {
         String location = "";
         String channel = "BHZ";
         String outFile = null;
+        String host = SeedlinkReader.DEFAULT_HOST;
+        int port = SeedlinkReader.DEFAULT_PORT;
         int maxRecords = 10;
         boolean verbose = false;
         DataOutputStream dos = null;
@@ -34,6 +40,10 @@ public class Client {
                 location = args[i+1];
             } else if (args[i].equals("-c")) {
                 channel = args[i+1];
+            } else if (args[i].equals("-h")) {
+                host = args[i+1];
+            } else if (args[i].equals("-p")) {
+                port = Integer.parseInt(args[i+1]);
             } else if (args[i].equals("-o")) {
                 outFile = args[i+1];
                 dos = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(outFile)));
@@ -42,37 +52,35 @@ public class Client {
                 if (maxRecords < 1) {maxRecords = 1;}
             } else if (args[i].equals("--verbose")) {
                 verbose = true;
+            } else if (args[i].equals("--version")) {
+                System.out.println(BuildVersion.getDetailedVersion());
+                System.exit(0);
+            } else if (args[i].equals("--help")) {
+                System.out.println("java "+Client.class.getName()+" [-n net][-s sta][-l loc][-c chan][-h host][-p port][-o outfile][-m maxpackets][--verbose][--version][--help]");
+                System.exit(0);
             }
         }
         List<String> config = new ArrayList<String>();
         config.add("STATION "+station+" "+network);
         config.add("SELECT "+location+channel+".D");
         config.add("DATA");
-        SeedlinkReader reader = new SeedlinkReader(config, "rtserve.iris.washington.edu", 18000, verbose);
+        SeedlinkReader reader = new SeedlinkReader(config, host, port, verbose);
         int i=0;
-        DataRecord firstdr = null;
-        Date firstDate = null;
-        SeedlinkPacket slp = null;
+        Writer out = new OutputStreamWriter(System.out);
         while(i<maxRecords) {
-            slp = reader.next();
-            if (firstdr == null) {
-                firstdr = reader.next().getMiniSeed();
-                firstDate = new Date();
-            }
+            SeedlinkPacket slp = reader.next();
             DataRecord dr = slp.getMiniSeed();
             if (dos != null) {
                 dr.write(dos);
             }
-            Blockette[] blockettes = dr.getBlockettes();
-            for (int j = 0; j < blockettes.length; j++) {
-                System.out.println("Blockette: "+blockettes[j].getType()+" "+blockettes[j].getName());
+            if (verbose) {
+                dr.writeASCII(out, "    ");
+                out.flush();
             }
             i++;
         }
-        dos.close();
-        DataRecord lastdr = slp.getMiniSeed();
-        System.out.println(firstDate+"  "+firstdr.getHeader().getEndTime());
-        System.out.println(new Date()+"  "+lastdr.getHeader().getEndTime());
+        if (dos != null) { dos.close(); }
         reader.close();
+        System.out.println("Finished: "+new Date());
     }
 }
