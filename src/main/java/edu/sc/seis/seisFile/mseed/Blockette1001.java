da1465f75b96cd0fd75b5157f0495db62afca45b
crotwell <crotwell@localhost>
2010-09-29 20:23:32 -0400

    [svn r21706] initial

---
+++ src/main/java/edu/sc/seis/seisFile/mseed/Blockette1001.java
@@ -0,0 +1,61 @@
+package edu.sc.seis.seisFile.mseed;
+
+import java.io.IOException;
+import java.io.Writer;
+
+
+public class Blockette1001 extends DataBlockette {
+
+    public static final int B1001_SIZE = 8;
+    
+    public Blockette1001() {
+        super(B1001_SIZE);
+    }
+
+    public Blockette1001(byte[] info, boolean swapBytes) {
+        super(info, swapBytes);
+        trimToSize(B1001_SIZE);
+    }
+
+    public int getSize() {
+        return B1001_SIZE;
+    }
+
+    public int getType() { return 1001; }
+
+    public String getName() {
+        return "Data Extension Blockette";
+    }
+    public byte getTimingQuality() {return info[4];}
+
+    public void setTimingQuality(byte  v) {
+        info[4] = v;
+    }
+
+    public byte getMicrosecond() {
+        return info[5];
+    }
+
+    public void setMicrosecond(byte  v) {
+        info[5] = v;
+    }
+    
+    public byte getReserved() {
+        return info[6];
+    }
+
+    public void setReserved(byte  v) {
+        info[6] = v;
+    }
+
+    public byte getFrameCount() {return info[7];}
+
+    public void setFrameCount(byte  v) {info[7] = v;}
+    
+    @Override
+    public void writeASCII(Writer out) throws IOException {
+        out.write("Blockette1001 "+getTimingQuality()+" "+getMicrosecond()+" "+getFrameCount());
+    }
+    
+    
+}
