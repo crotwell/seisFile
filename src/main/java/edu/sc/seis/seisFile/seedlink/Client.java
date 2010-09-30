358b28bf7a82ce1354b82e8f7851d2899b904756
crotwell <crotwell@localhost>
2010-09-30 16:20:02 -0400

    [svn r21713] clean up, info packets

--- src/main/java/edu/sc/seis/seisFile/seedlink/Client.java
+++ src/main/java/edu/sc/seis/seisFile/seedlink/Client.java
@@ -2,9 +2,11 @@ package edu.sc.seis.seisFile.seedlink;
 
 import java.io.BufferedOutputStream;
 import java.io.DataOutputStream;
+import java.io.EOFException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.io.Writer;
 import java.net.UnknownHostException;
@@ -31,6 +33,7 @@ public class Client {
         int maxRecords = 10;
         boolean verbose = false;
         DataOutputStream dos = null;
+        PrintWriter out = new PrintWriter(System.out, true);
         for (int i = 0; i < args.length; i++) {
             if (args[i].equals("-n")) {
                 network = args[i+1];
@@ -53,34 +56,57 @@ public class Client {
             } else if (args[i].equals("--verbose")) {
                 verbose = true;
             } else if (args[i].equals("--version")) {
-                System.out.println(BuildVersion.getDetailedVersion());
+                out.println(BuildVersion.getDetailedVersion());
                 System.exit(0);
             } else if (args[i].equals("--help")) {
-                System.out.println("java "+Client.class.getName()+" [-n net][-s sta][-l loc][-c chan][-h host][-p port][-o outfile][-m maxpackets][--verbose][--version][--help]");
+                out.println("java "+Client.class.getName()+" [-n net][-s sta][-l loc][-c chan][-h host][-p port][-o outfile][-m maxpackets][--verbose][--version][--help]");
                 System.exit(0);
             }
         }
-        List<String> config = new ArrayList<String>();
-        config.add("STATION "+station+" "+network);
-        config.add("SELECT "+location+channel+".D");
-        config.add("DATA");
-        SeedlinkReader reader = new SeedlinkReader(config, host, port, verbose);
+        SeedlinkReader reader = new SeedlinkReader(host, port, verbose);
+        if (verbose) {
+            reader.setVerboseWriter(out);
+            String[] lines = reader.sendHello();
+            out.println("line 1 :"+lines[0]);
+            out.println("line 2 :"+lines[1]);
+            out.flush();
+
+            reader.info("ID");
+            out.println("ID");
+            SeedlinkPacket infoPacket = reader.next(); // ID only returns 1 packet, others might return more, careful.
+            infoPacket.getMiniSeed().writeASCII(out, "    ");
+            out.println("    "+new String(infoPacket.getMiniSeed().getData()));
+        }
+        
+        reader.sendCmd("STATION "+station+" "+network);
+        reader.sendCmd("SELECT "+location+channel+".D");
+        // use one of DATA and FETCH
+        // DATA for realtime
+        // FETCH to start flow at a specific time
+        // DMC only goes back 48 hours so update date to something more recent
+       // reader.sendCmd("FETCH 0 2010,09,30,12,00,00"); 
+        reader.sendCmd("DATA");
+        reader.endHandshake(); // let 'er rip
         int i=0;
-        Writer out = new OutputStreamWriter(System.out);
-        while(i<maxRecords) {
-            SeedlinkPacket slp = reader.next();
-            DataRecord dr = slp.getMiniSeed();
-            if (dos != null) {
-                dr.write(dos);
+        try {
+            while((maxRecords == -1 || i<maxRecords) && reader.isConnected()) {
+                SeedlinkPacket slp = reader.next();
+                DataRecord dr = slp.getMiniSeed();
+                if (dos != null) {
+                    dr.write(dos);
+                }
+                if (dos == null || verbose) {
+                    // print something to the screen if we are not saving to disk
+                    dr.writeASCII(out, "    ");
+                    out.flush();
+                }
+                i++;
             }
-            if (verbose) {
-                dr.writeASCII(out, "    ");
-                out.flush();
-            }
-            i++;
+        } catch (EOFException e) {
+            // done I guess
         }
         if (dos != null) { dos.close(); }
         reader.close();
-        System.out.println("Finished: "+new Date());
+        out.println("Finished: "+new Date());
     }
 }
