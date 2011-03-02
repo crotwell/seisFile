78e4d70dda764c31c2f52574278c90ac9a7a5b8b
Philip Crotwell <crotwell@seis.sc.edu>
2011-03-02 07:38:37 -0500

    [svn r22160] test Btime compare

--- src/test/java/edu/sc/seis/seisFile/mseed/BtimeTest.java
+++ src/test/java/edu/sc/seis/seisFile/mseed/BtimeTest.java
@@ -9,4 +9,14 @@ public class BtimeTest extends TestCase {
         assertEquals(new Btime(), new Btime(new Btime().getAsBytes()));
     }
     
+    public void testGreaterThan() {
+        Btime s1 = new Btime(2011, 59, 17, 11, 3, 1750);
+        Btime s2 = new Btime(2011, 59, 17, 11, 13, 3500);
+        Btime p1 = new Btime(2011, 59, 17, 0, 8, 1000);
+        Btime p2 = new Btime(2011, 59, 17, 0, 18, 3250);
+        assertTrue(s1.greaterThan(p1));
+        assertTrue(s1.greaterThan(p2));
+        assertTrue(s2.greaterThan(p1));
+        assertTrue(s2.greaterThan(p2));
+    }
 }
