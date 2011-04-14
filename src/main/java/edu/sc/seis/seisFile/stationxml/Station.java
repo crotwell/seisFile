e37582deb5180981bb526c5259cb69ed31886aa4
crotwell <crotwell@localhost>
2011-04-14 14:35:57 -0400

    [svn r22304] new network tag in stationxml, clean up code

--- src/main/java/edu/sc/seis/seisFile/stationxml/Station.java
+++ src/main/java/edu/sc/seis/seisFile/stationxml/Station.java
@@ -5,64 +5,53 @@ import java.util.List;
 
 import javax.xml.stream.XMLEventReader;
 import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.StartElement;
 import javax.xml.stream.events.XMLEvent;
 
-
-
 public class Station {
-    
+
     public Station(XMLEventReader reader) throws XMLStreamException, StationXMLException {
-        XMLEvent cur = reader.peek();
-        if (cur.isStartElement() && cur.asStartElement().getName().getLocalPart().equals(StaMessage.STATION)) {
-            XMLEvent e = reader.nextEvent(); // pop Station
-            netCode = StaxUtil.pullAttribute(e.asStartElement(), StaMessage.NET_CODE);
-            staCode = StaxUtil.pullAttribute(e.asStartElement(), StaMessage.STA_CODE);
-            while(reader.hasNext()) {
-                e = reader.peek();
-                if (e.isStartElement()) {
-                    String elName = e.asStartElement().getName().getLocalPart();
-                    if (elName.equals("StationEpoch")) {
-                        staList.add(new StationEpoch(reader));
-                    } else {
-                        StaxUtil.skipToMatchingEnd(reader);
-                    }
-                } else if (e.isEndElement()) {
-                    reader.nextEvent();
-                    return;
-                } else  {
-                    e = reader.nextEvent();
+        StartElement startE = StaxUtil.expectStartElement(StationXMLTagNames.STATION, reader);
+        netCode = StaxUtil.pullAttribute(startE, StationXMLTagNames.NET_CODE);
+        staCode = StaxUtil.pullAttribute(startE, StationXMLTagNames.STA_CODE);
+        while (reader.hasNext()) {
+            XMLEvent e = reader.peek();
+            if (e.isStartElement()) {
+                String elName = e.asStartElement().getName().getLocalPart();
+                if (elName.equals("StationEpoch")) {
+                    staList.add(new StationEpoch(reader));
+                } else {
+                    StaxUtil.skipToMatchingEnd(reader);
                 }
+            } else if (e.isEndElement()) {
+                reader.nextEvent();
+                return;
+            } else {
+                e = reader.nextEvent();
             }
-        } else {
-            throw new StationXMLException("Not a Station element: "+cur.asStartElement().getName().getLocalPart());
         }
     }
-    
+
     List<StationEpoch> staList = new ArrayList<StationEpoch>();
-    
+
     String netCode, staCode;
 
-    
     public String getNetCode() {
         return netCode;
     }
 
-    
     public void setNetCode(String netCode) {
         this.netCode = netCode;
     }
 
-    
     public String getStaCode() {
         return staCode;
     }
 
-    
     public void setStaCode(String staCode) {
         this.staCode = staCode;
     }
 
-    
     public List<StationEpoch> getStationEpochs() {
         return staList;
     }
