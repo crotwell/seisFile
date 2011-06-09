ced9fde4f7bb09ec93b6bcbf40f66d9a974d3e3d
Philip Crotwell <crotwell@seis.sc.edu>
2011-06-09 09:17:14 -0400

    [svn r22398] almost have usgs CWB reader client working

--- src/main/java/edu/sc/seis/seisFile/dataSelectWS/Client.java
+++ src/main/java/edu/sc/seis/seisFile/dataSelectWS/Client.java
@@ -1,94 +1,37 @@
 package edu.sc.seis.seisFile.dataSelectWS;
 
-import java.io.BufferedOutputStream;
-import java.io.DataOutputStream;
-import java.io.FileOutputStream;
-import java.io.PrintWriter;
-import java.net.URL;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.List;
-import java.util.TimeZone;
-
-import edu.sc.seis.seisFile.BuildVersion;
-import edu.sc.seis.seisFile.mseed.DataRecord;
+import edu.sc.seis.seisFile.MSeedQueryClient;
+import edu.sc.seis.seisFile.MSeedQueryReader;
 
 
-public class Client {
+public class Client extends MSeedQueryClient {
 
+
+    protected Client(MSeedQueryReader reader) {
+        super(reader);
+    }
+    
+    
+
+    /**
+     * @param args
+     */
     public static void main(String[] args) throws Exception {
-        String network = "IU";
-        String station = "ANMO";
-        String location = "00";
-        String channel = "BHZ";
-        String outFile = null;
-        GregorianCalendar cal = new GregorianCalendar(TimeZone.getTimeZone("GMT"));
-        cal.add(cal.MINUTE, -10);
-        Date begin = cal.getTime();
-        Float duration = 600f;
-        DataSelectReader dsReader = new DataSelectReader();
-        int maxRecords = 10;
-        boolean verbose = false;
-        DataOutputStream dos = null;
-        PrintWriter out = new PrintWriter(System.out, true);
+        DataSelectReader reader = new DataSelectReader();
         for (int i = 0; i < args.length; i++) {
-            if (args[i].equals("-n")) {
-                network = args[i + 1];
-            } else if (args[i].equals("-s")) {
-                station = args[i + 1];
-            } else if (args[i].equals("-l")) {
-                location = args[i + 1];
-            } else if (args[i].equals("-c")) {
-                channel = args[i + 1];
-            } else if (args[i].equals("-u")) {
-                dsReader = new DataSelectReader(args[i + 1]);
-            } else if (args[i].equals("-b")) {
-                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
-                begin = dateFormat.parse(args[i + 1]);
-            } else if (args[i].equals("-d")) {
-                duration = Float.parseFloat(args[i + 1]);
-            } else if (args[i].equals("-o")) {
-                outFile = args[i + 1];
-                dos = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(outFile)));
-            } else if (args[i].equals("-m")) {
-                maxRecords = Integer.parseInt(args[i + 1]);
-                if (maxRecords < -1) {
-                    maxRecords = -1;
-                }
-            } else if (args[i].equals("--verbose")) {
-                verbose = true;
-            } else if (args[i].equals("--version")) {
-                out.println(BuildVersion.getDetailedVersion());
-                System.exit(0);
-            } else if (args[i].equals("--help")) {
-                out.println("java "
-                        + Client.class.getName()
-                        + " [-n net][-s sta][-l loc][-c chan][-b yyyy-MM-ddTHH:mm:ss.SSS][-d seconds][-u url][-o outfile][-m maxpackets][--verbose][--version][--help]");
-                System.exit(0);
+            if (args[i].equals("-u")) {
+                reader = new DataSelectReader(args[i+1]);
             }
         }
-        
-        URL requestURL = dsReader.createQuery(network, station, location, channel, begin, duration);
-        if (verbose) {
-            out.println("URL: "+requestURL);
-        }
-        List<DataRecord> data = dsReader.read(requestURL);
-        for (DataRecord dr : data) {
-            if (dos != null) {
-                dr.write(dos);
-            }
-            if (dos == null || verbose) {
-                // print something to the screen if we are not saving to
-                // disk
-                dr.writeASCII(out, "    ");
-                out.flush();
-            }
-        }
-        if (dos != null) {
-            dos.close();
-        }
-        out.println("Finished: " + new Date());
+        Client client = new Client(reader);
+        client.readData(args);
+    }
+
+    @Override
+    public String getHelp() {
+        return "java "
+        + Client.class.getName()
+        + " [-n net][-s sta][-l loc][-c chan][-b yyyy-MM-ddTHH:mm:ss.SSS][-d seconds][-u url][-o outfile][-m maxpackets][--verbose][--version][--help]";
     }
    
 }
---
+++ src/main/java/edu/sc/seis/seisFile/usgsCWB/CWBReader.java
@@ -0,0 +1,106 @@
+package edu.sc.seis.seisFile.usgsCWB;
+
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PushbackInputStream;
+import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.TimeZone;
+
+import edu.sc.seis.seisFile.MSeedQueryReader;
+import edu.sc.seis.seisFile.dataSelectWS.DataSelectException;
+import edu.sc.seis.seisFile.mseed.DataRecord;
+import edu.sc.seis.seisFile.mseed.SeedFormatException;
+import edu.sc.seis.seisFile.mseed.SeedRecord;
+
+
+public class CWBReader implements MSeedQueryReader {
+    
+    public CWBReader() {
+        this(DEFAULT_HOST);
+    }
+    
+    public CWBReader(String host) {
+        this(host, DEFAULT_PORT);
+    }
+    
+    public CWBReader(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    protected String createQuery(String network, String station, String location, String channel) throws IOException, DataSelectException, SeedFormatException {
+        String query = leftPad(network.trim(), 2);
+        query += leftPad(station.trim(), 5);
+        query += leftPad(location.trim(), 2);
+        query += leftPad(channel.trim(), 3);
+        return query;
+    }
+    
+    public String createQuery(String network, String station, String location, String channel, Date begin, float durationSeconds) throws IOException, DataSelectException, SeedFormatException {
+        String query = "'-s' '"+createQuery(network, station, location, channel)+"' ";
+        SimpleDateFormat longFormat = new SimpleDateFormat("yyyy/MM/dd-HH:mm:ss");
+        longFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
+        query += "'-b' '" + longFormat.format(begin)+"' ";
+        query += "'-d' '" + (int)Math.ceil(durationSeconds)+"' ";
+        query += "'-t' 'ms'";
+        query += "\t";
+        return query;
+    }
+    
+    public List<DataRecord> read(String query) throws IOException, DataSelectException, SeedFormatException {
+        Socket socket = new Socket(host, port);
+        socket.setReceiveBufferSize(512000);
+        OutputStream outtcp = socket.getOutputStream();
+        outtcp.write(query.getBytes());
+        outtcp.flush();
+        PushbackInputStream bif = new PushbackInputStream(new BufferedInputStream(socket.getInputStream()), 1);
+        DataInputStream in = new DataInputStream(bif);
+        List<DataRecord> records = new ArrayList<DataRecord>();
+        while (true) {
+            try {
+                int nextByte = bif.read();
+                if (nextByte == '<') {
+                    // end of stream marked with "<EOR>" so end read
+                    break;
+                } else {
+                    bif.unread(nextByte);
+                }
+                SeedRecord sr = SeedRecord.read(in);
+                if (sr instanceof DataRecord) {
+                    records.add((DataRecord)sr);
+                } else {
+                    System.err.println("None data record found, skipping...");
+                }
+            } catch (EOFException e) {
+                // end of data?
+                break;
+            }
+        }
+        in.close();
+        outtcp.close();
+        socket.close();
+        return records;
+    }
+    
+    protected String leftPad(String in, int length) {
+        if (in.length() == length) { 
+            return in; 
+        } else {
+            return leftPad(in+"@", length);
+        }
+    }
+
+    private String host;
+    private int port;
+    
+    public static final String DEFAULT_HOST = "cwb-pub.cr.usgs.gov";
+    
+    public static final int DEFAULT_PORT = 2061;
+}
