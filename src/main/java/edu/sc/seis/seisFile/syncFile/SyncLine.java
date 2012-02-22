5bbdd4f97e935af6c20e6235e636e8f8d3454cb7
Philip Crotwell <crotwell@seis.sc.edu>
2012-02-22 14:51:50 -0500

    reindent add Comparable add getters

--- src/main/java/edu/sc/seis/seisFile/syncFile/SyncLine.java
+++ src/main/java/edu/sc/seis/seisFile/syncFile/SyncLine.java
@@ -5,12 +5,11 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+public class SyncLine implements Comparable<SyncLine> {
 
-public class SyncLine {
-    
     public static SyncLine parse(String line) throws ParseException, NumberFormatException {
         String[] s = line.split("\\|", -1);
-       SyncLine out = new SyncLine(s[0], // net
+        SyncLine out = new SyncLine(s[0], // net
                                     s[1], // sta
                                     s[2], // loc
                                     s[3], // chan
@@ -28,17 +27,12 @@ public class SyncLine {
         return out;
     }
 
-    public SyncLine(SyncLine copy,
-                    Date startTime,
-                    Date endTime,
-                    Float samplesPerSecond) {
+    public SyncLine(SyncLine copy, Date startTime, Date endTime, Float samplesPerSecond) {
         this(copy, startTime, endTime);
         this.samplesPerSecond = samplesPerSecond;
     }
-    
-    public SyncLine(SyncLine copy,
-                    Date startTime,
-                    Date endTime) {
+
+    public SyncLine(SyncLine copy, Date startTime, Date endTime) {
         this(copy.net,
              copy.sta,
              copy.loc,
@@ -66,7 +60,7 @@ public class SyncLine {
                     Float samplesPerSecond) {
         this(net, sta, loc, chan, startTime, endTime, maxClockDrift, samplesPerSecond, "", "", "", "", "", null, null);
     }
-    
+
     public SyncLine(String net,
                     String sta,
                     String loc,
@@ -99,7 +93,6 @@ public class SyncLine {
         this.lineModByDMC = lineModByDMC;
         this.lineModByDCC = lineModByDCC;
     }
-    
 
     public String formatLine() {
         return concatWithSeparator(new String[] {net,
@@ -108,8 +101,8 @@ public class SyncLine {
                                                  chan,
                                                  dateToString(startTime),
                                                  dateToString(endTime),
-                                                 maxClockDrift==null?"":"" + maxClockDrift,
-                                                 samplesPerSecond==null?"":"" + samplesPerSecond,
+                                                 maxClockDrift == null ? "" : "" + maxClockDrift,
+                                                 samplesPerSecond == null ? "" : "" + samplesPerSecond,
                                                  channelFlag,
                                                  stationVolume,
                                                  dccTapeNumber,
@@ -119,18 +112,38 @@ public class SyncLine {
                                                  dateToString(lineModByDCC)}, SyncFile.SEPARATOR);
     }
 
-    /** returns true if line is from the same channel and immediately after this SyncLine. */
+    /**
+     * returns true if line is from the same channel and immediately after this
+     * SyncLine.
+     */
     public boolean isContiguous(SyncLine line, float tolerenceSeconds) {
-        return  net.equals(line.net) &&
-                sta.equals(line.sta) &&
-                loc.equals(line.loc) &&
-                chan.equals(line.chan) && 
-                (line.startTime.getTime() == endTime.getTime() || line.startTime.after(endTime)) && 
-                ((line.startTime.getTime()-endTime.getTime())/1000.0 + 1/samplesPerSecond) <= tolerenceSeconds;
+        return net.equals(line.net) && sta.equals(line.sta) && loc.equals(line.loc) && chan.equals(line.chan)
+                && (line.startTime.getTime() == endTime.getTime() || line.startTime.after(endTime))
+                && ((line.startTime.getTime() - endTime.getTime()) / 1000.0 + 1 / samplesPerSecond) <= tolerenceSeconds;
     }
 
     public SyncLine concat(SyncLine after) {
         return new SyncLine(this, this.startTime, after.endTime);
+    }
+    
+    public int compareTo(SyncLine two) {
+        int subComp = getNet().compareToIgnoreCase(two.getNet());
+        if (subComp != 0) {
+            return subComp;
+        }
+        subComp = getSta().compareToIgnoreCase(two.getSta());
+        if (subComp != 0) {
+            return subComp;
+        }
+        subComp = getLoc().compareToIgnoreCase(two.getLoc());
+        if (subComp != 0) {
+            return subComp;
+        }
+        subComp = getChan().compareToIgnoreCase(two.getChan());
+        if (subComp != 0) {
+            return subComp;
+        }
+        return getStartTime().compareTo(two.getStartTime());
     }
     
     public static String concatWithSeparator(String[] items, String separator) {
@@ -145,9 +158,11 @@ public class SyncLine {
     }
 
     /**
-     * partse the date from a string, checking for null and empty. If empty, a null Date
-     * is returned.
-     * @throws NumberFormatException if string can not be parsed
+     * partse the date from a string, checking for null and empty. If empty, a
+     * null Date is returned.
+     * 
+     * @throws NumberFormatException
+     *             if string can not be parsed
      */
     public static Float stringToFloat(String s) {
         if (s == null || s.length() == 0) {
@@ -157,9 +172,11 @@ public class SyncLine {
     }
 
     /**
-     * partse the date from a string, checking for null and empty. If empty, a null Date
-     * is returned.
-     * @throws ParseException if string is not of the form "yyyy,DDD,hh:mm:ss"
+     * partse the date from a string, checking for null and empty. If empty, a
+     * null Date is returned.
+     * 
+     * @throws ParseException
+     *             if string is not of the form "yyyy,DDD,hh:mm:ss"
      */
     public static Date stringToDate(String d) throws ParseException {
         if (d == null || d.length() == 0) {
@@ -186,20 +203,94 @@ public class SyncLine {
         DateFormat df = new SimpleDateFormat("yyyy,DDD,hh:mm:ss");
         return df.format(d);
     }
-    
+
+    public String getNet() {
+        return net;
+    }
+
+    public String getSta() {
+        return sta;
+    }
+
+    public String getLoc() {
+        return loc;
+    }
+
+    public String getChan() {
+        return chan;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public Float getMaxClockDrift() {
+        return maxClockDrift;
+    }
+
+    public Float getSamplesPerSecond() {
+        return samplesPerSecond;
+    }
+
+    public String getChannelFlag() {
+        return channelFlag;
+    }
+
+    public String getStationVolume() {
+        return stationVolume;
+    }
+
+    public String getDccTapeNumber() {
+        return dccTapeNumber;
+    }
+
+    public String getDmcTabpNumber() {
+        return dmcTabpNumber;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public Date getLineModByDMC() {
+        return lineModByDMC;
+    }
+
+    public Date getLineModByDCC() {
+        return lineModByDCC;
+    }
+
     String net;
+
     String sta;
+
     String loc;
+
     String chan;
+
     Date startTime;
+
     Date endTime;
+
     Float maxClockDrift;
+
     Float samplesPerSecond;
+
     String channelFlag;
+
     String stationVolume;
+
     String dccTapeNumber;
+
     String dmcTabpNumber;
+
     String comment;
+
     Date lineModByDMC;
+
     Date lineModByDCC;
 }
