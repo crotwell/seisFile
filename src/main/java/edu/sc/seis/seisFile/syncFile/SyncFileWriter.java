2f6c44d0f52159824fe8a68d37379233f070c608
Philip Crotwell <crotwell@seis.sc.edu>
2012-02-27 11:19:02 -0500

    initial

---
+++ src/main/java/edu/sc/seis/seisFile/syncFile/SyncFileWriter.java
@@ -0,0 +1,73 @@
+package edu.sc.seis.seisFile.syncFile;
+
+import java.io.PrintWriter;
+import java.util.Date;
+
+
+public class SyncFileWriter {
+    
+
+    public SyncFileWriter(String dccName, PrintWriter writer) {
+        this(dccName, SyncLine.dateToString(new Date()), new String[0], writer);
+    }
+    
+    public SyncFileWriter(String dccName, String dateModified, PrintWriter writer) {
+        this(dccName, dateModified, new String[0], writer);
+    }
+    
+    public SyncFileWriter(String dccName, String dateModified, String[] extraHeaders, PrintWriter writer) {
+        super();
+        this.dccName = dccName;
+        this.dateModified = dateModified;
+        this.extraHeaders = extraHeaders;
+        this.writer = writer;
+    }
+
+    public void appendAll(SyncFile sFile) {
+        appendAll(sFile, true);
+    }
+
+    public void appendAll(SyncFile sFile, boolean consolidate) {
+        for (SyncLine line : sFile.getSyncLines()) {
+            appendLine(line, consolidate);
+        }
+    }
+    
+    public void appendLine(SyncLine line) {
+        appendLine(line, true);
+    }
+
+    public void appendLine(SyncLine line, boolean consolidate) {
+        if (consolidate) {
+            if (previous == null) {
+                previous = line;
+            } else if (previous.isContiguous(line, SyncFile.DEFAULT_TOLERENCE)) {
+                previous = previous.concat(line);
+            } else {
+                writer.println(previous.formatLine());
+                previous = line;
+            }
+        } else {
+            writer.println(line.formatLine());
+        }
+    }
+    
+    public void close() {
+        if (previous != null) {
+            writer.println(previous.formatLine());
+        }
+        writer.close();
+    }
+    
+    
+    SyncLine previous = null;
+
+    String dccName;
+
+    String dateModified;
+
+    String[] extraHeaders;
+    
+    protected PrintWriter writer;
+    
+}
