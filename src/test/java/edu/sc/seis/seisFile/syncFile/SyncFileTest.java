d8559fdbae2ee9accc7294fb24d37c85b8f24d38
Philip Crotwell <crotwell@seis.sc.edu>
2012-02-29 09:13:38 -0500

    more test of date formatting and parsing

--- src/test/java/edu/sc/seis/seisFile/syncFile/SyncFileTest.java
+++ src/test/java/edu/sc/seis/seisFile/syncFile/SyncFileTest.java
@@ -2,7 +2,10 @@ package edu.sc.seis.seisFile.syncFile;
 
 import static org.junit.Assert.*;
 
+import java.text.DateFormat;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
 
 import org.junit.Test;
 
@@ -14,6 +17,15 @@ public class SyncFileTest {
         SyncLine sl = SyncLine.parse("CO|JSC|00|BHZ|2010,243,08:00:05|2010,243,08:00:13|0.0|100.0|||||||");
         sl = SyncLine.parse("IU|ANMO|01|BHE|1994,258,00:00:00|1994,265,00:00:00|.0005|20||CG|||||1998,275||");
         sl = SyncLine.parse("IU|ANMO|01|BHE|1994,265,00:00:00|1994,275,00:00:00|.0005|20||CG|||||1998,275||");
+        DateFormat dateFormat = new SimpleDateFormat("yyyy,DDD,HH:mm:ss");
+        dateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
+        assertEquals(sl.startTime, dateFormat.parse("1994,265,00:00:00"));
+        sl = SyncLine.parse("IU|ANMO|01|BHE|1994,265,00:00:00.000|1994,275,00:00:00|.0005|20||CG|||||1998,275||");
+        assertEquals(sl.startTime, dateFormat.parse("1994,265,00:00:00"));
+        DateFormat dayOnlyDateFormat = new SimpleDateFormat("yyyy,DDD");
+        dayOnlyDateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
+        assertEquals(sl.lineModByDCC, dayOnlyDateFormat.parse("1998,275"));
+        
     }
     
     @Test
