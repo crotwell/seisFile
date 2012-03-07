304762e690faef9e051f7798e5fb347808d18c8e
Philip Crotwell <crotwell@seis.sc.edu>
2012-03-07 11:03:33 -0500

    initial

---
+++ src/main/java/edu/sc/seis/seisFile/SeisFileRuntimeException.java
@@ -0,0 +1,20 @@
+package edu.sc.seis.seisFile;
+
+
+public class SeisFileRuntimeException extends RuntimeException {
+
+    public SeisFileRuntimeException() {
+    }
+
+    public SeisFileRuntimeException(String arg0) {
+        super(arg0);
+    }
+
+    public SeisFileRuntimeException(Throwable arg0) {
+        super(arg0);
+    }
+
+    public SeisFileRuntimeException(String arg0, Throwable arg1) {
+        super(arg0, arg1);
+    }
+}
