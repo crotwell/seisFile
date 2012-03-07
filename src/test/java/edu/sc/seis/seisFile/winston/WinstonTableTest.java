0ad97f3135a44deb87b66732a9e46fc14450ba97
Philip Crotwell <crotwell@seis.sc.edu>
2012-03-07 13:35:43 -0500

    fix test failure

--- src/test/java/edu/sc/seis/seisFile/winston/WinstonTableTest.java
+++ src/test/java/edu/sc/seis/seisFile/winston/WinstonTableTest.java
@@ -11,16 +11,18 @@ public class WinstonTableTest {
 
     @Test
     public void test() throws ParseException {
-        String prefix = "W_";
-        WinstonSCNL database = new WinstonSCNL(prefix+"BIRD$BHN$CO$00", prefix);
+        String prefix = "W";
+        String databaseName = "BIRD$BHN$CO$00";
+        WinstonSCNL database = new WinstonSCNL(WinstonUtil.prefixTableName(prefix, databaseName), prefix);
         String heliTable = "BIRD$BHN$CO$00$$H2010_04_12";
         String plainTable = "BIRD$BHN$CO$00$$2010_04_12";
         WinstonTable wt = new WinstonTable(database, heliTable);
+        assertEquals(prefix+"_"+databaseName, database.getDatabaseName());
         assertEquals(2010, wt.getYear());
         assertEquals(4, wt.getMonth());
         assertEquals(12, wt.getDay());
-        assertEquals(heliTable, wt.getHeliTableName());
         assertEquals(plainTable, wt.getTableName());
+        assertEquals(heliTable, wt.getHeliTableName());
         wt = new WinstonTable(database, plainTable);
         assertEquals(2010, wt.getYear());
         assertEquals(4, wt.getMonth());
