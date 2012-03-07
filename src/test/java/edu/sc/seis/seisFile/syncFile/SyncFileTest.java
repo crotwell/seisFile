bafcd32d33dbc0e4830ecf9b34ff94b75c9d6618
Philip Crotwell <crotwell@seis.sc.edu>
2012-03-07 11:06:35 -0500

    redo exceptions

--- src/test/java/edu/sc/seis/seisFile/syncFile/SyncFileTest.java
+++ src/test/java/edu/sc/seis/seisFile/syncFile/SyncFileTest.java
@@ -9,11 +9,13 @@ import java.util.TimeZone;
 
 import org.junit.Test;
 
+import edu.sc.seis.seisFile.SeisFileException;
+
 
 public class SyncFileTest {
 
     @Test
-    public void testParse() throws NumberFormatException, ParseException {
+    public void testParse() throws SeisFileException, ParseException {
         SyncLine sl = SyncLine.parse("CO|JSC|00|BHZ|2010,243,08:00:05|2010,243,08:00:13|0.0|100.0|||||||");
         sl = SyncLine.parse("IU|ANMO|01|BHE|1994,258,00:00:00|1994,265,00:00:00|.0005|20||CG|||||1998,275||");
         sl = SyncLine.parse("IU|ANMO|01|BHE|1994,265,00:00:00|1994,275,00:00:00|.0005|20||CG|||||1998,275||");
@@ -29,7 +31,7 @@ public class SyncFileTest {
     }
     
     @Test
-    public void testConcat() throws NumberFormatException, ParseException {
+    public void testConcat() throws SeisFileException {
         SyncLine first = SyncLine.parse("CO|JSC|00|BHZ|2010,243,08:00:05|2010,243,08:00:13|0.0|100.0|||||||");
         SyncLine second = SyncLine.parse("CO|JSC|00|BHZ|2010,243,08:00:13|2010,243,08:00:21|0.0|100.0|||||||");
         System.out.println(first.endTime.getTime()+"  "+second.startTime.getTime());
