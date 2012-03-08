4440fd4c1d85e7e187ef46430fcd24abe511b9c5
Philip Crotwell <crotwell@seis.sc.edu>
2012-03-07 21:17:28 -0500

    add endtime arg

--- src/main/java/edu/sc/seis/seisFile/QueryParams.java
+++ src/main/java/edu/sc/seis/seisFile/QueryParams.java
@@ -26,23 +26,9 @@ public class QueryParams {
             } else if (args[i].equals("-c")) {
                 channel = args[i + 1];
             } else if (args[i].equals("-b")) {
-                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSz");
-                dateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
-                String beginStr = args[i + 1].trim();
-                if (beginStr.matches(".+\\d")) {
-                    beginStr = beginStr + " GMT";
-                }
-                try {
-                    begin = dateFormat.parse(beginStr);
-                } catch(ParseException e) {
-                    dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssz");
-                    try {
-                        begin = dateFormat.parse(beginStr);
-                    } catch(ParseException ee) {
-                        throw new SeisFileException("Illegal date format, should be:  yyyy-MM-dd'T'HH:mm:ss or yyyy-MM-dd'T'HH:mm:ss.SSS",
-                                                    ee);
-                    }
-                }
+                begin = extractDate(args[i + 1]);
+            } else if (args[i].equals("-e")) {
+                end = extractDate(args[i + 1]);
             } else if (args[i].equals("-d")) {
                 duration = Float.parseFloat(args[i + 1]);
             } else if (args[i].equals("-o")) {
@@ -63,11 +49,15 @@ public class QueryParams {
         GregorianCalendar cal = new GregorianCalendar(TimeZone.getTimeZone("GMT"));
         cal.add(Calendar.SECOND, -1 * Math.round(duration));
         cal.add(Calendar.MILLISECOND, -1000 * Math.round(duration - Math.round(duration)));
-        if (begin == null) {begin = cal.getTime();}
-        cal.setTime(begin);
-        cal.add(Calendar.SECOND, Math.round(duration));
-        cal.add(Calendar.MILLISECOND, -1000 * Math.round(duration - Math.round(duration)));
-        end = cal.getTime();
+        if (begin == null) {
+            begin = cal.getTime();
+        }
+        if (end == null) {
+            cal.setTime(begin);
+            cal.add(Calendar.SECOND, Math.round(duration));
+            cal.add(Calendar.MILLISECOND, 1000 * Math.round(duration - Math.round(duration)));
+            end = cal.getTime();
+        }
     }
 
     String network;
@@ -149,5 +139,28 @@ public class QueryParams {
             dos = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(outFile)));
         }
         return dos;
+    }
+
+    Date extractDate(String dateString) throws SeisFileException {
+        dateString = dateString.trim();
+        SimpleDateFormat dateFormat;
+        if (dateString.length() == 23) {
+            dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSz");
+        } else if (dateString.length() == 19) {
+            dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssz");
+        } else {
+            // if (dateString.length() == 10)
+            dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        }
+        dateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
+        if (dateString.length() > 10 && dateString.matches(".+\\d")) {
+            dateString = dateString + " GMT";
+        }
+        try {
+            return dateFormat.parse(dateString);
+        } catch(ParseException e) {
+            throw new SeisFileException("Illegal date format, should be: yyyy-MM-dd or yyyy-MM-dd'T'HH:mm:ss or yyyy-MM-dd'T'HH:mm:ss.SSS",
+                                        e);
+        }
     }
 }
