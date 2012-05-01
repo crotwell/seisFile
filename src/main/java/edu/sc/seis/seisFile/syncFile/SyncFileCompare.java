9c32d1ee8e80f882789f5799f1c7ad5f8d756f02
Philip Crotwell <crotwell@seis.sc.edu>
2012-05-01 08:21:00 -0400

    initial output to gmt plot, cleanup

--- src/main/java/edu/sc/seis/seisFile/syncFile/SyncFileCompare.java
+++ src/main/java/edu/sc/seis/seisFile/syncFile/SyncFileCompare.java
@@ -1,7 +1,11 @@
 package edu.sc.seis.seisFile.syncFile;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
 import java.util.Collections;
 import java.util.Date;
 import java.util.Iterator;
@@ -10,10 +14,12 @@ import java.util.List;
 import edu.sc.seis.seisFile.SeisFileException;
 
 /**
- * Compares two sync files, creating 3 psuedo-sync files. 
- * <ul><li>In A and In B</li>
+ * Compares two sync files, creating 3 psuedo-sync files.
+ * <ul>
+ * <li>In A and In B</li>
  * <li>In A and Not In B</li>
- * <li>Not In A and In B</li></ul>
+ * <li>Not In A and In B</li>
+ * </ul>
  * 
  * @author crotwell
  * 
@@ -29,8 +35,8 @@ public class SyncFileCompare {
     void process() {
         String now = SyncLine.dateToString(new Date());
         inAinB = new SyncFile(a.dccName + " " + b.getDccName(), now, new String[] {"sync compare inAinB"});
-        notAinB = new SyncFile(a.dccName + " " + b.getDccName(), now, new String[] {"sync compare notAinB"});
-        inAnotB = new SyncFile(a.dccName + " " + b.getDccName(), now, new String[] {"sync compare inAnotB"});
+        notAinB = new SyncFile("not "+a.dccName + " in " + b.getDccName(), now, new String[] {"sync compare notAinB"});
+        inAnotB = new SyncFile("in "+a.dccName + " not " + b.getDccName(), now, new String[] {"sync compare inAnotB"});
         List<SyncLine> aLines = a.getSyncLines();
         Collections.sort(aLines);
         List<SyncLine> bLines = b.getSyncLines();
@@ -50,20 +56,16 @@ public class SyncFileCompare {
     }
 
     static SyncLine[] processItem(SyncLine aLine, SyncLine bLine, SyncFile inAinB, SyncFile notAinB, SyncFile inAnotB) {
-        System.out.println("Test: "+aLine+" "+bLine);
         if (aLine != null && bLine != null) {
             if (aLine.getEndTime().before(bLine.getStartTime())) {
                 // both sorted, so know only in A
                 inAnotB.addLine(aLine, true);
                 aLine = null;
-                System.out.println("put A into "+inAnotB.getDccName());
             } else if (bLine.getEndTime().before(aLine.getStartTime())) {
                 // both sorted, so know bL only in B
                 notAinB.addLine(bLine, true);
                 bLine = null;
-                System.out.println("put B into "+notAinB.getDccName());
             } else {
-                System.out.println("Overlap");
                 // some time overlap between aL and bL
                 if (aLine.getStartTime().equals(bLine.getStartTime()) && aLine.getEndTime().equals(bLine.getEndTime())) {
                     // same
@@ -90,8 +92,11 @@ public class SyncFileCompare {
                     }
                 } else if (bLine.getStartTime().before(aLine.getStartTime())) {
                     // part of b before a, swap order and reprocess
-                    SyncLine[] tmp = processItem(bLine, aLine, inAinB, inAnotB, notAinB);
-                    return new SyncLine[] {tmp[1], tmp[0]};
+                    SyncLine[] split = bLine.split(aLine.getStartTime());
+                    notAinB.addLine(split[0]);
+                    if (split.length != 1) {
+                        return processItem(aLine, split[1], inAinB, notAinB, inAnotB);
+                    }
                 }
             }
         } else if (aLine != null) {
@@ -126,6 +131,85 @@ public class SyncFileCompare {
         return inAnotB;
     }
 
+    public void outputToGMTScript(String filename) throws IOException {
+        Date earliest = null;
+        Date latest = null;
+        if ( ! getInAinB().isEmpty()) {
+            earliest = getInAinB().getEarliest();
+            latest = getInAinB().getLatest();
+        }
+        if ( ! getInAnotB().isEmpty() ) {
+            Date tmp = getInAnotB().getEarliest();
+            if (earliest == null || tmp.before(earliest)) {
+                earliest = tmp;
+            }
+            tmp = getInAnotB().getLatest();
+            if (latest == null || tmp.after(latest)) {
+                latest = tmp;
+            }
+        }
+        if ( ! getNotAinB().isEmpty() ) {
+            Date tmp = getNotAinB().getEarliest();
+            if (earliest == null || tmp.before(earliest)) {
+                earliest = tmp;
+            }
+            tmp = getNotAinB().getLatest();
+            if (latest == null || tmp.after(latest)) {
+                latest = tmp;
+            }
+        }
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
+        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(filename)));
+        out.println("BASE=\"sync\"");
+        out.println("PS=${BASE}.ps");
+        out.println();
+        String labelStep = "1Y";
+        if (latest.getTime()-earliest.getTime() < 1000*86400*180) {
+            labelStep = "1O";
+        } 
+        if (latest.getTime()-earliest.getTime() < 1000*86400*30) {
+            labelStep = "1D";
+        } 
+        if (latest.getTime()-earliest.getTime() < 1000*86400*1) {
+            labelStep = "1H";
+        } 
+        out.println("psbasemap -R"+sdf.format(earliest)+"/"+sdf.format(latest)+"/0/3 -JX6i -B"+labelStep+"/1 -K > $PS");
+        out.println("pstext -R -JX -Ggreen -O -K >> $PS <<END");
+        out.println(sdf.format(earliest)+" 2.7 12 0 1 LM Both");
+        out.println("END");
+
+        out.println("pstext -R -JX -Gred -O -K >> $PS <<END");
+        out.println(sdf.format(earliest)+" 2.5 12 0 1 LM "+getNotAinB().getDccName());
+        out.println("END");
+        out.println("pstext -R -JX -Gblue -O -K >> $PS <<END");
+        out.println(sdf.format(earliest)+" 2.3 12 0 1 LM "+getInAinB().getDccName());
+        out.println("END");
+        out.println("psxy -R -JX -Wobese,green -m -O -K >> $PS <<END");
+        for (SyncLine sline : getInAinB()) {
+            out.println("> ");
+            out.println(sdf.format(sline.getStartTime())+" "+2);
+            out.println(sdf.format(sline.getEndTime())+" "+2);
+        }
+        out.println("END");
+        out.println("psxy -R -JX -Wobese,red -m -O -K >> $PS <<END");
+        for (SyncLine sline : getNotAinB()) {
+            out.println("> ");
+            out.println(sdf.format(sline.getStartTime())+" "+1);
+            out.println(sdf.format(sline.getEndTime())+" "+1);
+        }
+        out.println("END");
+        out.println("psxy -R -JX -Wobese,blue -m -O -K >> $PS <<END");
+        for (SyncLine sline : getInAnotB()) {
+            out.println("> ");
+            out.println(sdf.format(sline.getStartTime())+" "+1);
+            out.println(sdf.format(sline.getEndTime())+" "+1);
+        }
+        out.println("END");
+        out.println("psxy -R -JX -O  >> $PS <<END");
+        out.println("END");
+        out.close();
+    }
+
     SyncFile a;
 
     SyncFile b;
@@ -136,17 +220,54 @@ public class SyncFileCompare {
 
     SyncFile inAnotB;
     
+    static void printUsage() {
+        System.err.println("Usage: syncFileCompare [--gmt] -a file1.sync -b file2.sync");
+    }
+    
+    static String trimDotSync(String filename) {
+        String fileBase = filename;
+        if (filename.endsWith(".sync")) {
+            fileBase = filename.substring(0, filename.lastIndexOf(".sync"));
+        }
+        return fileBase;
+    }
+
     public static void main(String[] args) throws IOException, SeisFileException {
-        if (args.length != 2) {
-            System.err.println("Usage: syncFileCompare file1.sync file2.sync");
+        boolean doGMT = false;
+        String file1Name = "", file2Name = "";
+        for (int i = 0; i < args.length; i++) {
+            if ("--gmt".equals(args[i])) {
+                doGMT = true;
+            } else if ("-a".equals(args[i])) {
+                file1Name = args[i+1];
+                i++;
+            } else if ("-b".equals(args[i])) {
+                file2Name = args[i+1];
+                i++;
+            } else {
+                System.err.println("I don't understand '"+args[i]+"'");
+                printUsage();
+                return;
+            }
+        }
+        if (file1Name.length() == 0 || file2Name.length() == 0) {
+            System.err.println("Both a and b filenames are required: '"+file1Name+"' '"+file2Name+"'");
+            printUsage();
             return;
         }
-        SyncFile file1 = SyncFile.load(new File(args[0]));
-        SyncFile file2 = SyncFile.load(new File(args[1]));
+        String file1Base = trimDotSync(file1Name);
+        String file2Base = trimDotSync(file2Name);
+        SyncFile file1 = SyncFile.load(new File(file1Name));
+        SyncFile file2 = SyncFile.load(new File(file2Name));
         SyncFileCompare sfc = new SyncFileCompare(file1, file2);
-        sfc.getInAinB().saveToFile("in_"+args[0]+"_in_"+args[1]+".sync");
-        sfc.getNotAinB().saveToFile("not_"+args[0]+"_in_"+args[1]+".sync");
-        sfc.getInAnotB().saveToFile("in_"+args[0]+"_not_"+args[1]+".sync");
-        System.out.println("Done: A: "+file1.getSyncLines().size()+" B: "+file2.getSyncLines().size()+" inAinB: "+sfc.getInAinB().getSyncLines().size()+" notAinB: "+sfc.getNotAinB().getSyncLines().size()+" inAnotB: "+sfc.getInAnotB().getSyncLines().size());
+        sfc.getInAinB().saveToFile("in_" + file1Base + "_in_" + file2Base + ".sync");
+        sfc.getNotAinB().saveToFile("not_" + file1Base + "_in_" + file2Base + ".sync");
+        sfc.getInAnotB().saveToFile("in_" + file1Base + "_not_" + file2Base + ".sync");
+        if (args[0].equals("--gmt")) {
+            sfc.outputToGMTScript("syncCompare.gmt");
+        }
+        System.out.println("Done: A: " + file1.getSyncLines().size() + " B: " + file2.getSyncLines().size()
+                + " inAinB: " + sfc.getInAinB().getSyncLines().size() + " notAinB: "
+                + sfc.getNotAinB().getSyncLines().size() + " inAnotB: " + sfc.getInAnotB().getSyncLines().size());
     }
 }
