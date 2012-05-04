c8635332be81176c68aaa03076af71698110bc78
Philip Crotwell <crotwell@seis.sc.edu>
2012-05-04 12:05:05 -0400

    some cmdline tests to run when releasing

---
+++ src/test/cmdline/waveserver.sh
@@ -0,0 +1,4 @@
+bin/waveserverclient --menu -h eeyore.seis.sc.edu -p 16022
+
+bin/waveserverclient -h eeyore.seis.sc.edu -p 16022 -n CO -s JSC -l 00 -c HHZ  -b 2012-01-01T12:34:56 -e 2012-01-01T12:56:32 -o waveserver.mseed --steim1 --recLen 8
+
