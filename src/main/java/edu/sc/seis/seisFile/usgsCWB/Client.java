1cb6cbc4aed5d549a518c9b0427bb48dec6c3bc8
Philip Crotwell <crotwell@seis.sc.edu>
2012-07-17 10:53:26 -0400

    add timeouts

--- src/main/java/edu/sc/seis/seisFile/usgsCWB/Client.java
+++ src/main/java/edu/sc/seis/seisFile/usgsCWB/Client.java
@@ -1,13 +1,28 @@
 package edu.sc.seis.seisFile.usgsCWB;
 
+import edu.sc.seis.seisFile.MSeedQueryClient;
 import edu.sc.seis.seisFile.QueryParams;
 import edu.sc.seis.seisFile.SeisFileException;
 
-public class Client extends edu.sc.seis.seisFile.MSeedQueryClient {
+public class Client extends MSeedQueryClient {
 
     protected Client(String[] args) throws SeisFileException {
         super(args);
-        reader = new CWBReader();
+        String host = CWBReader.DEFAULT_HOST;
+        int port = CWBReader.DEFAULT_PORT;
+        int timeoutSec = 120;
+        for (int i = 0; i < args.length; i++) {
+            if (i < args.length-1) {
+                if (args[i].equals("-h")) {
+                    host = args[i + 1];
+                } else if (args[i].equals("-p")) {
+                    port = Integer.parseInt(args[i + 1]);
+                } else if (args[i].equals("--timeout")) {
+                    timeoutSec = Integer.parseInt(args[i + 1]);
+                }
+            }
+        }
+        reader = new CWBReader(host, port, timeoutSec*1000);
     }
 
     /**
@@ -22,7 +37,7 @@ public class Client extends edu.sc.seis.seisFile.MSeedQueryClient {
     public String getHelp() {
         return "java "
         + Client.class.getName()
-        + " "+QueryParams.getStandardHelpOptions()+"[-h host][-p port]";
+        + " "+QueryParams.getStandardHelpOptions()+"[-h host][-p port][--timeout sec]";
     }
     
 }
