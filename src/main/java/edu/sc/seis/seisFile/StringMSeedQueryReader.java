468b41a2371eb3b194d3d3822f8fc91e2d8fe9fa
Philip Crotwell <crotwell@seis.sc.edu>
2012-09-12 13:43:52 -0400

    support for timing downloads default locid to space-space

--- src/main/java/edu/sc/seis/seisFile/StringMSeedQueryReader.java
+++ src/main/java/edu/sc/seis/seisFile/StringMSeedQueryReader.java
@@ -14,7 +14,17 @@ public abstract class StringMSeedQueryReader implements MSeedQueryReader {
             throws IOException, SeisFileException, SeedFormatException {
         String query = createQuery(network, station, location, channel, begin, end);
         logger.info("Request: " + query);
-        return read(query);
+        long before = System.currentTimeMillis();
+        List<DataRecord> out = read(query);
+        long after = System.currentTimeMillis();
+        if (isTimed()) {
+            for (DataRecord dr : out) {
+                bytesRead += dr.getRecordSize();
+            }
+            seconds = (after-before)/1000f;
+            System.out.println(bytesRead+" bytes in "+seconds+" sec ("+bytesRead/1000/seconds+" kb/s)");
+        }
+        return out;
     }
 
     public abstract String createQuery(String network,
@@ -22,7 +32,7 @@ public abstract class StringMSeedQueryReader implements MSeedQueryReader {
                                        String location,
                                        String channel,
                                        Date begin,
-                                       Date end) throws IOException, SeisFileException, SeedFormatException;
+                                       Date end) throws IOException, SeisFileException;
 
     public abstract List<DataRecord> read(String query) throws IOException, SeisFileException, SeedFormatException;
 
@@ -35,6 +45,21 @@ public abstract class StringMSeedQueryReader implements MSeedQueryReader {
     }
 
     private boolean verbose = false;
+    
+    private boolean timed = false;
+
+    
+    public boolean isTimed() {
+        return timed;
+    }
+
+    
+    public void setTimed(boolean timed) {
+        this.timed = timed;
+    }
+    
+    public int bytesRead = 0;
+    public float seconds = 0;
 
     private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(StringMSeedQueryReader.class);
 }
