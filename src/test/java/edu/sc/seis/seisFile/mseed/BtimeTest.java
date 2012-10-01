32df34c950776223f7db1104a84b20a88034d3d2
Philip Crotwell <crotwell@seis.sc.edu>
2012-10-01 14:13:37 -0400

    add constructor for double seconds, more tests, fix bug witl Calendar.HOUR vs. Calendar.HOUR_OF_DAY

--- src/test/java/edu/sc/seis/seisFile/mseed/BtimeTest.java
+++ src/test/java/edu/sc/seis/seisFile/mseed/BtimeTest.java
@@ -1,16 +1,22 @@
 package edu.sc.seis.seisFile.mseed;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
 
 
-public class BtimeTest extends TestCase {
+
+public class BtimeTest {
+    
+    @Test
     public void testEquals(){
         assertEquals(new Btime(), new Btime());
         assertEquals(new Btime(), new Btime(new Btime().getAsBytes()));
     }
     
+    @Test
     public void testGreaterThan() {
-        Btime s1 = new Btime(2011, 59, 17, 11, 3, 1750);
+        Btime s1 = new Btime(2011, 59, 17, 11, 3, 1751);
         Btime s2 = new Btime(2011, 59, 17, 11, 13, 3500);
         Btime p1 = new Btime(2011, 59, 17, 0, 8, 1000);
         Btime p2 = new Btime(2011, 59, 17, 0, 18, 3250);
@@ -19,4 +25,15 @@ public class BtimeTest extends TestCase {
         assertTrue(s2.after(p1));
         assertTrue(s2.after(p2));
     }
+    
+    @Test
+    public void testBtimeFromDouble() {
+        Btime s1 = new Btime(2011, 59, 17, 11, 3, 1751);
+        long millis = s1.convertToCalendar().getTimeInMillis();
+        double d = millis/1000.0; // double seconds
+        d += (s1.getTenthMilli() % 10) / 10000.0;
+        Btime out = new Btime(d);
+        assertEquals(s1, out);
+    }
+    
 }
