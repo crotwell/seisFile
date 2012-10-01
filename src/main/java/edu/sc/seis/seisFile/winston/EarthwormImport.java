de9c428a3f707e59ac5e6ca6bb542c6a93dd7aa7
Philip Crotwell <crotwell@seis.sc.edu>
2012-10-01 14:03:27 -0400

    get rid of EarthwormEscapeInputStream

--- src/main/java/edu/sc/seis/seisFile/winston/EarthwormImport.java
+++ src/main/java/edu/sc/seis/seisFile/winston/EarthwormImport.java
@@ -1,11 +1,12 @@
 package edu.sc.seis.seisFile.winston;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 
 public class EarthwormImport {
     
-    public EarthwormImport(EarthwormEscapeInputStream in) {
+    public EarthwormImport(InputStream in) {
         this.in = in;
     }
     
@@ -13,20 +14,33 @@ public class EarthwormImport {
         byte[] buffer = new byte[BUFFER_SIZE];
         int index = 0;
         int nextByte = in.read();
+        boolean isEscapedByte = false;
         while(nextByte != EarthwormExport.STX) {
             nextByte = in.read();
         }
-        while(nextByte != EarthwormExport.ETX) {
+        // got a start, now read data
+        nextByte = in.read();
+        if (nextByte == EarthwormExport.ESC) {
+            isEscapedByte = true;
+            nextByte = in.read();
+        }
+        
+        while( isEscapedByte || nextByte != EarthwormExport.ETX) {
             buffer[index] = (byte)nextByte;
             index++;
+            isEscapedByte = false;
             nextByte = in.read();
+            if (nextByte == EarthwormExport.ESC) {
+                isEscapedByte = true;
+                nextByte = in.read();
+            }
         }
         byte[] tbBuf = new byte[index];
         System.arraycopy(buffer, 0, tbBuf, 0, tbBuf.length);
         return new EarthwormMessage(tbBuf);
     }
     
-    EarthwormEscapeInputStream in;
+    InputStream in;
     
     int BUFFER_SIZE = 4096*2;
 }
