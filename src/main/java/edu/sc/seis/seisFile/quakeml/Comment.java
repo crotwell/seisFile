185cc72931661cb6f52c454e11b300315e1b8ac3
Philip Crotwell <crotwell@seis.sc.edu>
2013-02-25 17:19:27 -0500

    initial quakeml 1.2 reading

---
+++ src/main/java/edu/sc/seis/seisFile/quakeml/Comment.java
@@ -0,0 +1,50 @@
+package edu.sc.seis.seisFile.quakeml;
+
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.StartElement;
+import javax.xml.stream.events.XMLEvent;
+
+import edu.sc.seis.seisFile.stationxml.StationXMLException;
+import edu.sc.seis.seisFile.stationxml.StaxUtil;
+
+
+public class Comment {
+    
+    public Comment(final XMLEventReader reader) throws XMLStreamException, StationXMLException {
+        StaxUtil.skipToStartElement(reader);
+        StartElement startE = StaxUtil.expectStartElement(QuakeMLTagNames.eventParameter, reader);
+        while (reader.hasNext()) {
+            XMLEvent e = reader.peek();
+            if (e.isStartElement()) {
+                String elName = e.asStartElement().getName().getLocalPart();
+                if (elName.equals(QuakeMLTagNames.text)) {
+                    text = StaxUtil.pullText(reader, QuakeMLTagNames.text);
+                } else if (elName.equals(QuakeMLTagNames.creationInfo)) {
+                    creationInfo = new CreationInfo(reader);
+                } else {
+                    StaxUtil.skipToMatchingEnd(reader);
+                }
+            } else if (e.isEndElement()) {
+                reader.nextEvent();
+                return;
+            } else {
+                e = reader.nextEvent();
+            }
+        }
+    }
+    
+    
+    public String getText() {
+        return text;
+    }
+
+    
+    public CreationInfo getCreationInfo() {
+        return creationInfo;
+    }
+
+    String text;
+
+    CreationInfo creationInfo;
+}
---
+++ src/main/java/edu/sc/seis/seisFile/quakeml/EventParameters.java
@@ -0,0 +1,78 @@
+package edu.sc.seis.seisFile.quakeml;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.StartElement;
+import javax.xml.stream.events.XMLEvent;
+
+import edu.sc.seis.seisFile.SeisFileException;
+import edu.sc.seis.seisFile.stationxml.StationXMLException;
+import edu.sc.seis.seisFile.stationxml.StaxUtil;
+
+
+public class EventParameters {
+    
+    public EventParameters(final XMLEventReader reader) throws XMLStreamException, SeisFileException {
+        this.reader = reader;
+        StaxUtil.skipToStartElement(reader);
+        StartElement startE = StaxUtil.expectStartElement(QuakeMLTagNames.eventParameter, reader);
+        while (reader.hasNext()) {
+            XMLEvent e = reader.peek();
+            if (e.isStartElement()) {
+                String elName = e.asStartElement().getName().getLocalPart();
+                System.out.println("EventParameters peek: "+elName);
+                if (elName.equals(QuakeMLTagNames.description)) {
+                    description = StaxUtil.pullText(reader, QuakeMLTagNames.description);
+                } else if (elName.equals(QuakeMLTagNames.creationInfo)) {
+                    creationInfo = new CreationInfo(reader);
+                } else if (elName.equals(QuakeMLTagNames.comment)) {
+                    commentList.add(new Comment(reader));
+                } else if (elName.equals(QuakeMLTagNames.event)) {
+                    events = new EventIterator(reader);
+                    break;
+                } else {
+                    System.out.println("In EventParameters Skipping: "+elName);
+                    StaxUtil.skipToMatchingEnd(reader);
+                }
+            } else if (e.isEndElement()) {
+                reader.nextEvent();
+                return;
+            } else {
+                e = reader.nextEvent();
+            }
+        }
+    }
+    
+    
+    public String getDescription() {
+        return description;
+    }
+
+    
+    public CreationInfo getCreationInfo() {
+        return creationInfo;
+    }
+
+    
+    public EventIterator getEvents() {
+        return events;
+    }
+
+    
+    public List<Comment> getCommentList() {
+        return commentList;
+    }
+
+    String description;
+
+    CreationInfo creationInfo;
+    
+    EventIterator events;
+    
+    List<Comment> commentList = new ArrayList<Comment>();
+    
+    XMLEventReader reader;
+}
