2c461e6b7cd306191f69d0abebbaa949fc2f98f7
Philip Crotwell <crotwell@seis.sc.edu>
2013-03-15 08:40:03 -0400

    fix feCode parsing, is in EventDescription

--- src/main/java/edu/sc/seis/seisFile/quakeml/EventDescription.java
+++ src/main/java/edu/sc/seis/seisFile/quakeml/EventDescription.java
@@ -1,19 +1,29 @@
 package edu.sc.seis.seisFile.quakeml;
 
+import javax.xml.namespace.QName;
 import javax.xml.stream.XMLEventReader;
 import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.Attribute;
 import javax.xml.stream.events.StartElement;
 import javax.xml.stream.events.XMLEvent;
 
-import edu.sc.seis.seisFile.stationxml.StationXMLException;
+import edu.sc.seis.seisFile.SeisFileException;
 import edu.sc.seis.seisFile.stationxml.StaxUtil;
 
 
 public class EventDescription {
     
-    public EventDescription(final XMLEventReader reader) throws XMLStreamException, StationXMLException {
+    public EventDescription(final XMLEventReader reader) throws XMLStreamException, SeisFileException {
         StaxUtil.skipToStartElement(reader);
         StartElement startE = StaxUtil.expectStartElement(QuakeMLTagNames.description, reader);
+        Attribute feCodeAttr = startE.getAttributeByName(new QName(QuakeMLTagNames.irisNameSpace, QuakeMLTagNames.fecode));
+        if (feCodeAttr != null) {
+            try {
+                irisFECode = Integer.parseInt(feCodeAttr.getValue());
+            } catch(NumberFormatException e) {
+                throw new SeisFileException("Unable to parse FECode, expected integer but was: "+feCodeAttr.getValue(), e);
+            }
+        }
         while (reader.hasNext()) {
             XMLEvent e = reader.peek();
             if (e.isStartElement()) {
@@ -46,6 +56,12 @@ public class EventDescription {
     public String getType() {
         return type;
     }
+    
+    public int getIrisFECode() {
+        return irisFECode;
+    }
+
+    int irisFECode = -1;
 
     String text, type;
 }
