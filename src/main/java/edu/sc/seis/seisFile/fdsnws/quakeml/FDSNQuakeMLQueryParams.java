d92dec27f3ca12301a9353f244a29e149c37b7f6
Philip Crotwell <crotwell@seis.sc.edu>
2013-04-15 10:50:28 -0400

    capitalize constants, constructors

--- src/main/java/edu/sc/seis/seisFile/fdsnws/quakeml/FDSNQuakeMLQueryParams.java
+++ src/main/java/edu/sc/seis/seisFile/fdsnws/quakeml/FDSNQuakeMLQueryParams.java
@@ -3,146 +3,152 @@ package edu.sc.seis.seisFile.fdsnws.quakeml;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-public class FDSNQuakeMLQueryParams {
+import edu.sc.seis.seisFile.fdsnws.AbstractQueryParams;
+
+public class FDSNQuakeMLQueryParams extends AbstractQueryParams{
 
     public FDSNQuakeMLQueryParams() {
-        // TODO Auto-generated constructor stub
+        this(IRIS_BASE_URI);
+    }
+    
+    public FDSNQuakeMLQueryParams(URI baseUri) {
+        super(baseUri);
     }
 
     /**
      * Limit to events on or after the specified start time .
      */
-    public static final String starttime = "starttime";
+    public static final String STARTTIME = "starttime";
 
     /**
      * Limit to events on or before the specified end time .
      */
-    public static final String endtime = "endtime";
+    public static final String ENDTIME = "endtime";
 
     /**
      * Limit to events with a latitude larger than the specified minimum.
      */
-    public static final String minlatitude = "minlatitude";
+    public static final String MINLATITUDE = "minlatitude";
 
     /**
      * Limit to events with a latitude smaller than the specified maximum.
      */
-    public static final String maxlatitude = "maxlatitude";
+    public static final String MAXLATITUDE = "maxlatitude";
 
     /**
      * Limit to events with a longitude larger than the specified minimum.
      */
-    public static final String minlongitude = "minlongitude";
-
-    public static final String maxlongitude = "maxlongitude";
+    public static final String MINLONGITUDE = "minlongitude";
 
     /**
      * Limit to events with a longitude smaller than the specified maximum.
      */
+    public static final String MAXLONGITUDE = "maxlongitude";
+
     /**
      * Specify the latitude to be used for a radius search.
      */
-    public static final String latitude = "latitude";
+    public static final String LATITUDE = "latitude";
 
     /** Specify the longitude to the used for a radius search. */
-    public static final String longitude = "longitude";
+    public static final String LONGITUDE = "longitude";
 
     /**
      * Limit to events within the specified minimum number of degrees from the
      * geographic point defined by the latitude and longitude parameters.
      */
-    public static final String minradius = "minradius";
+    public static final String MINRADIUS = "minradius";
 
     /**
      * Lim it to events within the specified maximum number of degrees from the
      * geographic point defined by the latitude and longitude parameters.
      */
-    public static final String maxradius = "maxradius";
+    public static final String MAXRADIUS = "maxradius";
 
     /**
      * Limit to events with depth more than the specified minimum.
      */
-    public static final String mindepth = "mindepth";
+    public static final String MINDEPTH = "mindepth";
 
     /**
      * Limit to events with depth less than the specified maximum .
      */
-    public static final String maxdepth = "maxdepth";
+    public static final String MAXDEPTH = "maxdepth";
 
     /**
      * Limit to events with a magnitude larger than the specified minimum.
      */
-    public static final String minmagnitude = "minmagnitude";
+    public static final String MINMAGNITUDE = "minmagnitude";
 
     /** Limit to events with a magnitude smaller than the specified maximum. */
-    public static final String maxmagnitude = "maxmagnitude";
+    public static final String MAXMAGNITUDE = "maxmagnitude";
 
     /**
      * Specify a magnitude type to use for testing the minimum and maximum
      * limits.
      */
-    public static final String magnitudetype = "magnitudetype";
+    public static final String MAGNITUDETYPE = "magnitudetype";
 
     /**
      * Specify if all origins for the event should be included, default is data
      * center dependent but is suggested to be the preferred origin only.
      */
-    public static final String includeallorigins = "includeallorigins";
+    public static final String INCLUDEALLORIGINS = "includeallorigins";
 
     /**
      * Specify if all magnitudes for the event should be included, default is
      * data center dependent but is suggested to be the preferred magnitude
      * only.
      */
-    public static final String includeallmagnitudes = "includeallmagnitudes";
+    public static final String INCLUDEALLMAGNITUDES = "includeallmagnitudes";
 
     /** Specify if phase arrivals should be included. */
-    public static final String includearrivals = "includearrivals";
+    public static final String INCLUDEARRIVALS = "includearrivals";
 
     /**
      * Select a specific event by ID; event identifier s are data center
      * specific.
      */
-    public static final String eventid = "eventid";
+    public static final String EVENTID = "eventid";
 
     /** Limit the results to the specified number of events. */
-    public static final String limit = "limit";
+    public static final String LIMIT = "limit";
 
     /**
      * Return results starting at the event count specified , starting at 1 .
      */
-    public static final String offset = "offset";
+    public static final String OFFSET = "offset";
 
     /**
-     * Order the result by time or magnitude with the following possibilities:
-     * time : order by origin descending time time - asc : order by origin
-     * ascending time magnitude : order by descending magnitude magnitude - asc
-     * : order by ascending magnitude
+     * Order the result by time or magnitude with the following possibilities:<br/>
+     * time : order by origin descending time <br/>
+     * time - asc : order by origin ascending time <br/>
+     * magnitude : order by descending magnitude <br/>
+     * magnitude - asc : order by ascending magnitude
      */
-    public static final String orderby = "orderby";
+    public static final String ORDERBY = "orderby";
 
     /**
      * Limit to events from a specified catalog
      */
-    public static final String catalog = "catalog";
+    public static final String CATALOG = "catalog";
 
     /**
      * Limit to events contributed by a specified contributor .
      */
-    public static final String contributor = "contributor";
+    public static final String CONTRIBUTOR = "contributor";
 
     /** Limit to events updated after the specified time. */
-    public static final String updatedafter = "updatedafter";
+    public static final String UPDATEDAFTER = "updatedafter";
 
     public static final String IRIS_BASE_URL = "http://service.iris.edu/fdsnws/event/1/query?";
-    
+
     public static final URI IRIS_BASE_URI;
-    
     static {
         try {
             IRIS_BASE_URI = new URI(IRIS_BASE_URL);
-        } catch (URISyntaxException e) {
-            throw new RuntimeException("Should no happen, bad default uri string"+IRIS_BASE_URL);
+        } catch(URISyntaxException e) {
+            throw new RuntimeException("Should no happen, bad default uri string" + IRIS_BASE_URL);
         }
     }
 }
