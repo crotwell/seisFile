06ce8bb53f82daf683e9cc16821f5396973a29e5
Philip Crotwell <crotwell@seis.sc.edu>
2013-04-15 10:50:28 -0400

    capitalize constants, constructors

--- src/main/java/edu/sc/seis/seisFile/fdsnws/dataSelectWS/FDSNDataSelectQueryParams.java
+++ src/main/java/edu/sc/seis/seisFile/fdsnws/dataSelectWS/FDSNDataSelectQueryParams.java
@@ -18,22 +18,22 @@ public class FDSNDataSelectQueryParams extends AbstractQueryParams {
     /**
      * Limit results to time series samples on or after the specified start time
      */
-    public static final String starttime = "starttime";
+    public static final String STARTTIME = "starttime";
 
     /** Limit results to time series samples on or before the specified end time */
-    public static final String endtime = "endtime";
+    public static final String ENDTIME = "endtime";
 
     /**
      * Select one or more network codes. Can be SEED network codes or data
      * center defined codes. Multiple codes are comma - separated.
      */
-    public static final String network = "network";
+    public static final String NETWORK = "network";
 
     /**
      * Select one or more SEED station codes. Multiple codes are comma -
      * separated.
      */
-    public static final String station = "station";
+    public static final String STATION = "station";
 
     /**
      * Select one or more SEED location identifiers. Multiple identifiers are
@@ -41,28 +41,28 @@ public class FDSNDataSelectQueryParams extends AbstractQueryParams {
      * translated to a string of two space characters to match blank location
      * IDs.
      */
-    public static final String location = "location";
+    public static final String LOCATION = "location";
 
     /**
      * Select one or more SEED channel codes. Multiple codes are comma -
      * separated.
      */
-    public static final String channel = "channel";
+    public static final String CHANNEL = "channel";
 
     /**
      * Select a specific SEED quality indicato r, handling is data center
      * dependent.
      */
-    public static final String quality = "quality";
+    public static final String QUALITY = "quality";
 
     /**
      * Limit results to continuous data segments of a minimum length specified
      * in seconds,
      */
-    public static final String minimumlength = "minimumlength";
+    public static final String MINIMUMLENGTH = "minimumlength";
 
     /** Limit results to the longest continuous segment per channel. */
-    public static final String longestonly = "longestonly";
+    public static final String LONGESTONLY = "longestonly";
     
 
     public static final String IRIS_BASE_URL = "http://service.iris.edu/fdsnws/dataselect/1/query?";
