43d31616e5b045d7a4da0b3972947dfd46a9e627
Philip Crotwell <crotwell@seis.sc.edu>
2013-04-19 13:50:00 -0400

    add DataSelectClient

---
+++ src/main/java/edu/sc/seis/seisFile/fdsnws/dataSelectWS/DataRecordIterator.java
@@ -0,0 +1,54 @@
+package edu.sc.seis.seisFile.fdsnws.dataSelectWS;
+
+import java.io.DataInput;
+import java.io.EOFException;
+import java.io.IOException;
+
+import edu.sc.seis.seisFile.mseed.DataRecord;
+import edu.sc.seis.seisFile.mseed.SeedFormatException;
+import edu.sc.seis.seisFile.mseed.SeedRecord;
+
+public class DataRecordIterator {
+
+    public DataRecordIterator(DataInput in) {
+        this.in = in;
+    }
+
+    public boolean hasNext() throws SeedFormatException, IOException {
+        if (in == null) {
+            return false;
+        }
+        while (in != null && nextDr == null) {
+            try {
+                SeedRecord sr = SeedRecord.read(in);
+                if (sr instanceof DataRecord) {
+                    nextDr = (DataRecord)sr;
+                    break;
+                } else {
+                    logger.warn("Not a data record, skipping..." + sr.getControlHeader().getSequenceNum() + " "
+                            + sr.getControlHeader().getTypeCode());
+                }
+            } catch(EOFException e) {
+                in = null;
+                nextDr = null;
+                break;
+            }
+        }
+        return nextDr != null;
+    }
+
+    public DataRecord next() throws SeedFormatException, IOException {
+        if (hasNext()) {
+            DataRecord out = nextDr;
+            nextDr = null;
+            return out;
+        }
+        return null;
+    }
+
+    DataRecord nextDr;
+
+    DataInput in;
+    
+    private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(DataRecordIterator.class);
+}
