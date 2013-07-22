1713b074b5da2413b85672635c8cec5fb31aaaa6
Philip Crotwell <crotwell@seis.sc.edu>
2013-07-22 14:42:38 -0400

    only append if value not already in param list

--- src/test/java/edu/sc/seis/seisFile/fdsnws/AbstractQueryParamsTest.java
+++ src/test/java/edu/sc/seis/seisFile/fdsnws/AbstractQueryParamsTest.java
@@ -25,8 +25,36 @@ public class AbstractQueryParamsTest {
         aqp.appendToParam(T, first);
         assertEquals("first", first, aqp.getParam(T));
         aqp.appendToParam(T, second);
-        assertEquals("first", first+","+second, aqp.getParam(T));
+        assertEquals("second", first+","+second, aqp.getParam(T));
         aqp.appendToParam(T, third);
-        assertEquals("first", first+","+second+","+third, aqp.getParam(T));
+        assertEquals("third", first+","+second+","+third, aqp.getParam(T));
+    }
+    
+    @Test
+    public void testRepeatAppendToParam() throws URISyntaxException {
+        String T = "TEST";
+        String first = "first";
+        String second = "second";
+        String third = "third";
+        AbstractQueryParams aqp = new AbstractQueryParams("test.seis.sc.edu") {
+            @Override
+            public String getServiceName() {
+                return "event";
+            }
+        };
+        aqp.appendToParam(T, first);
+        assertEquals("first", first, aqp.getParam(T));
+        aqp.appendToParam(T, second);
+        assertEquals("second", first+","+second, aqp.getParam(T));
+        aqp.appendToParam(T, third);
+        assertEquals("third", first+","+second+","+third, aqp.getParam(T));
+        // repeat, so not appended
+        String expected = first+","+second+","+third;
+        aqp.appendToParam(T, first);
+        assertEquals("repeat first", expected, aqp.getParam(T));
+        aqp.appendToParam(T, second);
+        assertEquals("repeat first", expected, aqp.getParam(T));
+        aqp.appendToParam(T, third);
+        assertEquals("repeat first", expected, aqp.getParam(T));
     }
 }
