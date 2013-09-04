303af0ec0d0a01244634c58d756666984276159d
Philip Crotwell <crotwell@seis.sc.edu>
2013-09-04 11:02:08 -0400

    add validation to test

--- src/test/java/edu/sc/seis/seisFile/fdsnws/quakeml/USGSExampleTest.java
+++ src/test/java/edu/sc/seis/seisFile/fdsnws/quakeml/USGSExampleTest.java
@@ -10,32 +10,41 @@ import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
 
 import org.junit.Test;
+import org.xml.sax.SAXException;
 
 import edu.sc.seis.seisFile.SeisFileException;
+import edu.sc.seis.seisFile.fdsnws.FDSNEventQuerier;
 import edu.sc.seis.seisFile.fdsnws.stationxml.StationXMLException;
 
 
 public class USGSExampleTest {
 
     @Test
-    public void test() throws IOException, SeisFileException, XMLStreamException, StationXMLException {
+    public void test() throws IOException, SeisFileException, XMLStreamException, StationXMLException, SAXException {
         String[] filenames = new String[] {"usgs/B000I89Z20Long.quakeml", 
                                            "usgs/C000HX2T18Long.quakeml", 
                                            "usgs/C000HVSF38Long.quakeml", 
                                            "usgs/C000HZFK62Long.quakeml"};
         for (String filename : filenames) {
-            
+            try {
         URL url = QuakeMLTest.loadResourceURL(filename);
         
         XMLInputFactory factory = XMLInputFactory.newInstance();
         XMLEventReader r = factory.createXMLEventReader(url.toString(), QuakeMLTest.loadResource(filename));
-        
+        FDSNEventQuerier.validateQuakeML(factory.createXMLStreamReader(url.toString(), QuakeMLTest.loadResource(filename)));
         Quakeml qml = new Quakeml(r);
+        assertTrue("sceham version", qml.checkSchemaVersion());
+        
         EventParameters ep = qml.getEventParameters();
         EventIterator it = ep.getEvents();
         while (it.hasNext()) {
             System.out.println(it.next());
         }
+            } catch(Exception ex) {
+                ex.printStackTrace(System.err);
+                fail("Trouble with "+filename);
+                
+            }
         }
     }
 }
