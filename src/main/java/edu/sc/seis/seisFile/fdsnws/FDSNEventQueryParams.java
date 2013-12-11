e1baba51179aa34b9db8b707a416f3620f34da88
Philip Crotwell <crotwell@seis.sc.edu>
2013-12-11 14:55:31 -0500

    add setter for IncludeAllMagnitudes

--- src/main/java/edu/sc/seis/seisFile/fdsnws/FDSNEventQueryParams.java
+++ src/main/java/edu/sc/seis/seisFile/fdsnws/FDSNEventQueryParams.java
@@ -275,6 +275,10 @@ public class FDSNEventQueryParams extends AbstractQueryParams implements Cloneab
 
     /** Specify if all magnitudes for the event should be included, default is data center dependent but is suggested to be the preferred magnitude only.
      */
+    public FDSNEventQueryParams setIncludeAllMagnitudes(String value) {
+        setParam(INCLUDEALLMAGNITUDES, value);
+        return this;
+    }
     public FDSNEventQueryParams setIncludeAllMagnitudes(boolean value) {
         setParam(INCLUDEALLMAGNITUDES, value);
         return this;
