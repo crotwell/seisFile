02377d780b0ffed68a88627656466911d2da10cf
Philip Crotwell <crotwell@seis.sc.edu>
2017-06-19 11:09:25 -0400

    add validation methods and constructor

--- src/main/java/edu/sc/seis/seisFile/fdsnws/stationxml/Response.java
+++ src/main/java/edu/sc/seis/seisFile/fdsnws/stationxml/Response.java
@@ -14,6 +14,14 @@ import edu.sc.seis.seisFile.fdsnws.stationxml.StationXMLException;
 import edu.sc.seis.seisFile.fdsnws.stationxml.StationXMLTagNames;
 
 public class Response {
+    
+    public Response(List<ResponseStage> responseStageList,
+                    InstrumentSensitivity instrumentSensitivity,
+                    InstrumentPolynomial instrumentPolynomial) {
+        this.responseStageList = responseStageList;
+        this.instrumentSensitivity = instrumentSensitivity;
+        this.instrumentPolynomial = instrumentPolynomial;
+    }
 
     public Response(XMLEventReader reader) throws XMLStreamException, StationXMLException {
         StartElement startE = StaxUtil.expectStartElement(StationXMLTagNames.RESPONSE, reader);
@@ -39,6 +47,63 @@ public class Response {
             }
         }
     }
+    
+    public static boolean isValid(InstrumentSensitivity sens) {
+        return sens != null && sens.frequency >= 0 && sens.sensitivityValue != -1;
+    }
+    
+    public static boolean isValid(Response resp) {
+        return resp != null && resp.responseStageList.size() != 0 && isValid(resp.instrumentSensitivity);
+    }
+    
+    public static void checkResponse(Response resp) throws InvalidResponse {
+        if ( ! isValid(resp)) {
+            if (resp.responseStageList.size() == 0) {
+                throw new InvalidResponse("response is not valid, zero stages");
+            } else if (resp.instrumentSensitivity.frequency < 0) {
+                throw new InvalidResponse("response is not valid, sensitivity frequency negative");
+            } else if (resp.instrumentSensitivity.sensitivityValue == -1) {
+                throw new InvalidResponse("response is not valid, sensitivity factor = -1");
+            } else {
+                ResponseStage stageZero = resp.responseStageList.get(0);
+                if (stageZero.getStageSensitivity().sensitivityValue == 1 && 
+                        stageZero.getResponseItem() instanceof PolesZeros &&
+                        ((PolesZeros)stageZero.getResponseItem()).getPoleList().size() == 0 &&
+                                ((PolesZeros)stageZero.getResponseItem()).getZeroList().size() == 0 ) {
+                throw new InvalidResponse("response is not valid, stage[0] gain = 1, no poles, no zeros, marker for \"UNKNOWN\"");
+                }
+            }
+            throw new InvalidResponse("Response invalid, reason unknown");
+        }
+    }
+
+
+    /**
+     * Checks for nonsense sensitivity (overall gain of -1) and trys to repair by multiplying the
+     * gains of the individual stages. This only works if all the frequencys are either the same
+     * or zero. We assume a frequency of zero means that there is no frequnecy dependence for this
+     * stage. 
+     */
+    public static void repairResponse(Response resp) throws InvalidResponse {
+        if(isValid(resp)) {
+            return;
+        }
+        List<ResponseStage> stageList = resp.responseStageList;
+        float sensitivity = 1;
+        float stageFreq = 0;
+        for (ResponseStage responseStage : stageList) {
+            if (stageFreq == 0) {stageFreq = responseStage.getStageSensitivity().getFrequency();}
+            if (responseStage.getStageSensitivity().getFrequency() != 0 
+                    && stageFreq != responseStage.getStageSensitivity().getFrequency()) {
+                throw new InvalidResponse("No sensitivity and different frequencies in the stages of the response. Stage 0="
+                    +stageFreq+"  stage "+responseStage.getNumber()+"= "+responseStage.getStageSensitivity().getFrequency());
+            }
+            sensitivity *= responseStage.getStageSensitivity().sensitivityValue;
+        }
+        resp.instrumentSensitivity.sensitivityValue = sensitivity;
+        resp.instrumentSensitivity.frequency = stageFreq;
+    }
+
 
     public List<ResponseStage> getResponseStageList() {
         return responseStageList;
