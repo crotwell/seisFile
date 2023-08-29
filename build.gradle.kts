61352f0d25b5428d6e486478c9cdd18fe88ee7fb
Philip Crotwell <crotwell@seis.sc.edu>
2023-08-29 16:33:36 -0400

    dep version updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -112,12 +112,12 @@ dependencies {
     implementation("edu.sc.seis:seedCodec:1.1.1")
     clientImplementation("info.picocli:picocli:4.7.5")
 
-    annotationProcessor("info.picocli:picocli-codegen:4.7.4")
+    annotationProcessor("info.picocli:picocli-codegen:4.7.5")
     implementation( "org.slf4j:slf4j-api:1.7.36")
     clientImplementation( "org.slf4j:slf4j-reload4j:1.7.36")
     implementation( "com.fasterxml.woodstox:woodstox-core:6.5.1")
     implementation( "org.apache.httpcomponents:httpclient:4.5.14")
-    implementation("org.json:json:20220924")
+    implementation("org.json:json:20230618")
 
     // Use JUnit Jupiter API for testing.
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
